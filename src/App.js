133954ee422d28854838852b53d9bacb8dd4d12e
Ernest Opara <ernest.ebuka@gmail.com>
2023-08-01 13:17:01 -0500

    Updated Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import EmailSignatureTemplate from './components/emailSignatureTemplate/EmailSignatureTemplate';
+import Form from './components/forms/Forms';
 
-function App() {
+const App = () => {
+  const [formData, setFormData] = useState(null);
+
+  const handleSubmitForm = (data) => {
+    setFormData(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Email Signature Generator</h1>
+      <Form onSubmit={handleSubmitForm} />
+      {formData && (
+        <EmailSignatureTemplate data={formData} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/forms/Forms.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Card, Form, Button } from 'react-bootstrap';
+import './Forms.css';
+import logoImage from '../../assets/tflogo.png'
+
+const EmailSignatureForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [role, setRole] = useState('');
+  const [email, setEmail] = useState('');
+  const [emailPicture, setEmailPicture] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      name,
+      role,
+      email,
+      emailPicture,
+    };
+    onSubmit(formData);
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    const imageURL = URL.createObjectURL(file);
+    setEmailPicture(imageURL);
+  };
+
+  return (
+    <Card className="form-card">
+      {/* Add logo at the top */}
+      <Card.Img variant="top" src={logoImage} className="form-logo" />
+      <Card.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Role</Form.Label>
+            <Form.Control
+              type="text"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Email Picture</Form.Label>
+            <Form.Control
+              type="file"
+              name="emailPicture"
+              onChange={handleImageChange}
+              required
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default EmailSignatureForm;
