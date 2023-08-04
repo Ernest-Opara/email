40caa7af977997d48fccdf40a55640cb0941a1b9
Ernest Opara <ernest.ebuka@gmail.com>
2023-08-03 23:36:32 -0500

    Created optional fields

--- src/components/emailSignatureTemplate/EmailSignatureTemplate.js
+++ src/components/emailSignatureTemplate/EmailSignatureTemplate.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { Card } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebook,
   faInstagram,
@@ -8,77 +8,150 @@ import {
   faLinkedin,
   faTwitter,
   faYoutube,
-} from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faPhone, faMapMarkerAlt, faLink } from '@fortawesome/free-solid-svg-icons';
-import './EmailSignatureTemplate.css';
-import logoImage from '../../assets/tflogo.png'
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+  faPhone,
+  faMapMarkerAlt,
+  faLink,
+} from "@fortawesome/free-solid-svg-icons";
+import "./EmailSignatureTemplate.css";
+import logoImage from "../../assets/tflogo.png";
 
 const EmailSignatureTemplate = ({ data }) => {
-  const { name, role, email, emailPicture, pNumber } = data;
+  const { name, role, email, emailPicture, pNumber, bLink } = data;
   const hasPersonalNumber = !!pNumber;
-  
+  const hasBookingLink = !!bLink;
 
   return (
-    <div className={`email-signature-template ${hasPersonalNumber ? 'has-personal-number' : ''}`}>
-      <Card className="left-half">
-        <div className="profile-picture">
-          <img src={emailPicture} alt="Profile" />
-        </div>
-        <div className="company-logo">
-          <img src={logoImage} alt="Company Logo" />
-        </div>
-      </Card>
-      <Card className="right-half">
-        <Card.Body>
-        <div className='name'>
-          <Card.Title>{name}</Card.Title></div>
-          <Card.Subtitle className="mb-2 text-muted">{role}</Card.Subtitle>
-          <Card.Text>ThriveFuel Marketing</Card.Text>
-          <div className="contact-info">
-            <div className="contact-item">
-              <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
-              <a href={`mailto:${email}`}>{email}</a>
+    <div
+      className={`signature-card ${
+        hasPersonalNumber || hasBookingLink ? "has-additional-fields" : ""
+      }`}
+    >
+      
+        <Card className="left-half">
+          <div className="profile-picture">
+            <img src={emailPicture} alt="Profile" />
+          </div>
+          <div className="company-logo">
+            <img src={logoImage} alt="Company Logo" />
+          </div>
+        </Card>
+
+        <Card className="right-half">
+          <Card.Body>
+            <div className="name">
+              <Card.Title>{name}</Card.Title>
             </div>
-            <div className='contact-item'>{pNumber && (
-              <p className="user-phone">
+            <Card.Subtitle className="mb-2 text-muted">{role}</Card.Subtitle>
+            <Card.Text>ThriveFuel Marketing</Card.Text>
+            <div className="contact-info">
+              <div className="contact-item">
+                <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+                <a href={`mailto:${email}`}>{email}</a>
+              </div>
+              <div className="contact-item">
+                {pNumber && (
+                  <p className="user-phone">
+                    <FontAwesomeIcon icon={faPhone} className="contact-icon" />
+                    <a href={pNumber}>{pNumber}</a>
+                  </p>
+                )}{" "}
+              </div>
+              <div className="contact-item">
                 <FontAwesomeIcon icon={faPhone} className="contact-icon" />
-                <a href='tel:{pNumber}'>{pNumber}</a>
-              </p>
-          )} </div>
-            <div className="contact-item">
-              <FontAwesomeIcon icon={faPhone} className="contact-icon" />
-              <a href='tel:833-239-3183' >833-239-3183</a>
+                <a href="tel:833-239-3183">833-239-3183</a>
+              </div>
+              <div className="contact-item">
+                {bLink && (
+                  <p className="user-phone">
+                    <FontAwesomeIcon icon={faLink} className="contact-icon" />
+                    <a href={bLink} target="_blank" rel="noopener noreferrer">
+                      Book time with me!
+                    </a>
+                  </p>
+                )}{" "}
+              </div>
+              <div className="contact-item">
+                <FontAwesomeIcon
+                  icon={faMapMarkerAlt}
+                  className="contact-icon"
+                />
+                <a href="https://goo.gl/maps/1dvEwQAuiTFTHYnu8">
+                  4611 E Airline Rd, Ste 200 Victoria, TX 77904{" "}
+                </a>
+              </div>
+              <div className="contact-item">
+                <FontAwesomeIcon icon={faLink} className="contact-icon" />
+                <a href="https://www.thrivefuel.com">
+                  <strong>thrivefuel.com</strong>
+                </a>
+              </div>
             </div>
-            <div className="contact-item">
-              <FontAwesomeIcon icon={faMapMarkerAlt} className="contact-icon" />
-              <a href='https://goo.gl/maps/1dvEwQAuiTFTHYnu8' >4611 E Airline Rd, Ste 200 Victoria, TX 77904 </a>
+            <div className="social-media-icons">
+              <a
+                href="https://www.facebook.com/your-facebook-url"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  className="social-media-icon"
+                />
+              </a>
+              <a
+                href="https://www.instagram.com/your-instagram-url"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  className="social-media-icon"
+                />
+              </a>
+              <a
+                href="https://www.tiktok.com/your-tiktok-url"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faTiktok}
+                  className="social-media-icon"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/your-linkedin-url"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  className="social-media-icon"
+                />
+              </a>
+              <a
+                href="https://twitter.com/your-twitter-url"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="social-media-icon"
+                />
+              </a>
+              <a
+                href="https://www.youtube.com/your-youtube-url"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  className="social-media-icon"
+                />
+              </a>
             </div>
-            <div className="contact-item">
-              <FontAwesomeIcon icon={faLink} className="contact-icon" />
-              <a href='https://www.thrivefuel.com'><strong>thrivefuel.com</strong></a>
-            </div>
-          </div>
-          <div className="social-media-icons">
-            <a href="https://www.facebook.com/your-facebook-url" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faFacebook} className="social-media-icon" />
-            </a>
-            <a href="https://www.instagram.com/your-instagram-url" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faInstagram} className="social-media-icon" />
-            </a>
-            <a href="https://www.tiktok.com/your-tiktok-url" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTiktok} className="social-media-icon" />
-            </a>
-            <a href="https://www.linkedin.com/your-linkedin-url" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faLinkedin} className="social-media-icon" />
-            </a>
-            <a href="https://twitter.com/your-twitter-url" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faTwitter} className="social-media-icon" />
-            </a>
-            <a href="https://www.youtube.com/your-youtube-url" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faYoutube} className="social-media-icon" />
-            </a>
-          </div>
-        </Card.Body>
+          </Card.Body>
       </Card>
     </div>
   );
--- src/components/forms/Forms.js
+++ src/components/forms/Forms.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import { Card, Form, Button } from 'react-bootstrap';
-import './Forms.css';
-import logoImage from '../../assets/tflogo.png'
+import React, { useState } from "react";
+import { Card, Form, Button, Row, Col } from "react-bootstrap";
+import "./Forms.css";
+import logoImage from "../../assets/tflogo.png";
+import profileImagePlaceholder from "../../assets/image.png";
 
 const EmailSignatureForm = ({ onSubmit }) => {
-  const [name, setName] = useState('');
-  const [role, setRole] = useState('');
-  const [pNumber, setpNumber] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [pNumber, setpNumber] = useState("");
+  const [bLink, setbLink] = useState("");
+  const [email, setEmail] = useState("");
   const [emailPicture, setEmailPicture] = useState(null);
 
   const handleSubmit = (e) => {
@@ -16,6 +18,7 @@ const EmailSignatureForm = ({ onSubmit }) => {
       name,
       role,
       pNumber,
+      bLink,
       email,
       emailPicture,
     };
@@ -29,60 +32,89 @@ const EmailSignatureForm = ({ onSubmit }) => {
   };
 
   return (
-    <Card className="form-card">
-      <Card.Img variant="top" src={logoImage} className="form-logo" />
-      <Card.Body>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
+    <div className="container">
+      <Row className="equal-height">
+        {/* Left Column: Picture */}
+        <Col xs={12} md={6}>
+          <div className="profile-picture-container">
+            <img
+              src={profileImagePlaceholder} // Replace this with the image URL from the form
+              alt="Profile"
+              className="profile-image"
             />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Role</Form.Label>
-            <Form.Control
-              type="text"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Personal Number</Form.Label>
-            <Form.Control
-              type="text"
-              value={pNumber}
-              onChange={(e) => setpNumber(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Email Picture</Form.Label>
-            <Form.Control
-              type="file"
-              name="emailPicture"
-              onChange={handleImageChange}
-              required
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
+          </div>
+        </Col>
+        <Col xs={12} md={6}>
+          <Card className="form-card">
+            <Card.Img variant="top" src={logoImage} className="form-logo" />
+            <Card.Body>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="data">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    placeholder="Enter your name"
+                  />
+                </Form.Group>
+                <Form.Group className="data">
+                  <Form.Label>Role</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                    required
+                    placeholder="Enter your role"
+                  />
+                </Form.Group>
+                <Form.Group className="data">
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    placeholder="Enter your email"
+                  />
+                </Form.Group>
+                <Form.Group className="data">
+                  <Form.Label>Personal Number</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={pNumber}
+                    onChange={(e) => setpNumber(e.target.value)}
+                    placeholder="Enter your personal number (Optional)"
+                  />
+                </Form.Group>
+                <Form.Group className="data">
+                  <Form.Label>Booking Link</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={bLink}
+                    onChange={(e) => setbLink(e.target.value)}
+                    placeholder="Enter your booking link (Optional)"
+                  />
+                </Form.Group>
+                <Form.Group className="data">
+                  <Form.Label>Email Picture</Form.Label>
+                  <Form.Control
+                    type="file"
+                    name="emailPicture"
+                    onChange={handleImageChange}
+                    required
+                  />
+                </Form.Group >
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
